84e0d89430fe87638fdabf9179f84c3c39d4e26d
daniel <daniel21dev@gmail.com>
2022-06-03 23:42:33 -0500

    Initial commit

---
+++ test/probando-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ProbandoCdk from '../lib/probando-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/probando-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ProbandoCdk.ProbandoCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
